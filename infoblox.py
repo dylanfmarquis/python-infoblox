779071ba731cf1ac66f1c65218552ea67f6500b7
dylanfmarquis <dylanfmarquis@dylanfmarquis.com>
2016-09-08 15:16:47 -0400

    Removed whitespace for PEP8

--- infoblox.py
+++ infoblox.py
@@ -238,7 +238,7 @@ class infoblox(object):
         handle = self._cname(self, name)
         return handle
 
-    
+
     def mx(self, name):
         """
         mx - MX record (Mail Exchanger) object
@@ -1012,34 +1012,34 @@ class infoblox(object):
             payload = '{{"target": "{0}", "weight": {1}, "name": "{2}", "priority": {3}'\
                       ',"port": {4}}}'\
                       .format(target, weight, self.name, priority, self.port)
-            resp = self.infoblox_.post('record:srv',payload)                                       
-            if resp.status_code != 201:                                                             
-                try:                                                                                
+            resp = self.infoblox_.post('record:srv',payload)
+            if resp.status_code != 201:
+                try:
                     return self.infoblox_.__caller__('Error creating srv record {0}'\
                                                      ' - Status: {1}'\
-                                                     .format(self.name, resp.status_code),  
-                                                     resp.status_code)                              
-                except Exception as e:                                                              
-                    return resp.status_code                                                         
-            return 0  
+                                                     .format(self.name, resp.status_code),
+                                                     resp.status_code)
+                except Exception as e:
+                    return resp.status_code
+            return 0
 
-        def delete(self):                                                                           
-            """                                                                                     
-            del - Delete a SRV record within Infoblox                                              
-                                                                                                    
-            input   void (void)                                                                     
-            output  0 (int)             Successful deletion                                        
-                    errno (int)         Error code of API call                                      
-            """                                                                                     
-            resp = self.infoblox_.delete(self._ref)                                                 
+        def delete(self):
+            """
+            del - Delete a SRV record within Infoblox
+
+            input   void (void)
+            output  0 (int)             Successful deletion
+                    errno (int)         Error code of API call
+            """
+            resp = self.infoblox_.delete(self._ref)
             if resp.status_code != 200:
-                try:                                                                                
+                try:
                     return self.infoblox_.__caller__('Error deleting SRV record {0} for {1}'\
                                                      ' - Status: {2}'\
-                                                     .format(self.name, resp.status_code),      
-                                                     resp.status_code)                              
-                except Exception as e:                                                              
-                    return resp.status_code                                                         
+                                                     .format(self.name, resp.status_code),
+                                                     resp.status_code)
+                except Exception as e:
+                    return resp.status_code
             return 0
 
         def update(self, target=None, weight=None, priority=None):
@@ -1059,13 +1059,13 @@ class infoblox(object):
             payload = '{{"target": "{0}", "weight": {1}, "name": "{2}", "priority": {3}'\
                       ',"port": {4}}}'\
                       .format(target, weight, self.name, priority, self.port)
-            resp = self.infoblox_.put(self._ref, payload)                                       
+            resp = self.infoblox_.put(self._ref, payload)
             if resp.status_code != 200:
-                try:                                                                                
+                try:
                     return self.infoblox_.__caller__('Error updating srv record {0}'\
                                                      ' - Status: {1}'\
-                                                     .format(self.name, resp.status_code),  
-                                                     resp.status_code)                              
-                except Exception as e:                                                              
-                    return resp.status_code                                                         
-            return 0 
+                                                     .format(self.name, resp.status_code),
+                                                     resp.status_code)
+                except Exception as e
+                    return resp.status_code
+            return 0
