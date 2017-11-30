17d5dcaf5fadba4aab310aedfaca69b4d0af4f36
Drew Monroe <d.monroe@snet.net>
2017-11-30 08:55:47 -0500

    Fix clobberin issue, add update function

--- infoblox/_internal/rpz_cname.py
+++ infoblox/_internal/rpz_cname.py
@@ -13,9 +13,10 @@ class _rpz_cname(object):
         """
         self.infoblox_ = infoblox_
         self.name = name
-        self._ref = self._ref()
+        self._ref = self.__ref()
+        self.zone = None
 
-    def _ref(self):
+    def __ref(self):
         """
         _ref - Get _ref for a specified CNAME record
 
@@ -34,7 +35,7 @@ class _rpz_cname(object):
         input   void (void)
         output  resp (parsed json)      Parsed JSON response
         """
-        resp = self.infoblox_.get('record:rpz:cname?name~={0}'.format(self.name))
+        resp = self.infoblox_.get('record:rpz:cname?name~={0}&_return_fields=canonical,comment,disable,name,rp_zone,ttl,view,zone'.format(self.name))
         if resp.status_code != 200:
             try:
                 return self.infoblox_.__caller__(
@@ -79,6 +80,11 @@ class _rpz_cname(object):
                     .format(self.name, resp.status_code), resp.status_code)
             except Exception:
                 return resp.status_code
+
+        # If we added successfully, set the zone
+        self.zone = rp_zone
+        self._ref = self.__ref()
+
         return 0
 
     def delete(self):
@@ -98,18 +104,39 @@ class _rpz_cname(object):
                 return resp.status_code
         return 0
 
-    def update(self, name, rp_zone, comment="", TTL=None, view=None):
+    def update(self, name=None, canonical=None,
+               comment="", ttl=None, view=None):
         """
         add - Create RPZ CNAME record
 
-        input   canonical (string)      Canonical address for CNAME record
-                rp_zone (string)        Response policy zone name
+        input   canonical (string)      Optional: Canonical address for CNAME
+                                                  record
+                rp_zone (string)        Optional: Response policy zone name
                 comment (string)        Optional: An optional comment
                 ttl (int)               Optional: Time to live
                 view (string)           Optional: The view where the record is
         output  0 (int)                 Success
         """
-        pass
-        # TODO: Build payload
-        # TODO: Make _PUT request
-        # TODO: Check status code and return/error out
+
+        kwargs = {
+                  "name": (name + '.' + self.zone if name is not None and
+                                                    self.zone is not None
+                                                  else None),
+                  "canonical": canonical,
+                  "comment": comment,
+                  "ttl": ttl,
+                  "view": view
+                 }
+        d = {key: kwargs[key] for key in kwargs if kwargs[key]}
+        payload = json.dumps(d)
+
+        resp = self.infoblox_.put(self._ref, payload)
+
+        if resp.status_code != 200:
+            try:
+                return self.infoblox_.__caller__(
+                    'Could not create CNAME record for {0} - Status {1}'
+                    .format(self.name, resp.status_code), resp.status_code)
+            except Exception:
+                return resp.status_code
+        return 0
